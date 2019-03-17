566cc6fa2da826224dd31f13807d7be87cca41b5
Vaibhav Andhare <vaibhavandhare7@gmail.com>
2019-03-17 20:52:16 +0530

    Private Policy and Terms & Conditions are added.

--- app/src/main/java/in/indekode/sayhey/StartActivity.java
+++ app/src/main/java/in/indekode/sayhey/StartActivity.java
@@ -1,10 +1,15 @@
 package in.indekode.sayhey;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,6 +18,7 @@ public class StartActivity extends AppCompatActivity {
 
     Button startLogin, startReg;
     FirebaseAuth auth;
+    TextView privacy_policy, termsAC;
 
     @Override
     protected void onStart() {
@@ -47,5 +53,64 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(StartActivity.this, RegisterActivity.class));
             }
         });
+
+        privacy_policy = findViewById(R.id.Private_policy);
+        privacy_policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(StartActivity.this);
+                alert.setTitle("Privacy Policy");
+
+                WebView wv = new WebView(StartActivity.this);
+                wv.loadUrl("http://indekode.ml/SayHey/privacy_policy.html");
+                wv.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+
+                        return true;
+                    }
+                });
+
+                alert.setView(wv);
+                alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        });
+
+        termsAC = findViewById(R.id.TermsAndCondn);
+        termsAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(StartActivity.this);
+                alert.setTitle("Terms & Conditions");
+
+                WebView wv = new WebView(StartActivity.this);
+                wv.loadUrl("http://indekode.ml/SayHey/terms_and_conditions.html");
+                wv.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+
+                        return true;
+                    }
+                });
+
+                alert.setView(wv);
+                alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        });
+
     }
 }
